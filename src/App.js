4b6525521787c2c5b5ebba227d397ba388e2be82
AbdullohKomil <akomiljonov@icloud.com>
2023-01-24 00:21:54 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react';
+import './assets/styles/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { List } from './component/List';
+import { Item } from './component/Item/';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem('todos')) || []
+  );
+
+  const inputValue = useRef();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: todos.at(-1)?.id + 1 || 1,
+        isCompleted: false,
+        text: inputValue.current.value,
+      },
+    ]);
+    inputValue.current.value = '';
+    toast('todo qoshildi!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='display-2 fw-bold text-center my-3'>TODO APP</h1>
+      <form
+        onSubmit={handleSubmit}
+        className='w-50 mx-auto p-5 shadow'
+      >
+        <div className='input-group'>
+          <input
+            required
+            ref={inputValue}
+            type='text'
+            className='form-control'
+            placeholder='Todo...'
+          />
+          <button
+            className='btn btn-primary'
+            type='submit'
+          >
+            SEND
+          </button>
+        </div>
+      </form>
+      {todos.length ? (
+        <List>
+          {todos.map((el) => (
+            <Item
+              key={el.id}
+              todos={todos}
+              setTodos={setTodos}
+              id={el.id}
+              isCompleted={el.isCompleted}
+              text={el.text}
+            />
+          ))}
+        </List>
+      ) : (
+        <h2 className='text-center h2 mt-5'>Todolar mavjus emas</h2>
+      )}
+      <ToastContainer
+        position='top-right'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
     </div>
   );
 }
---
+++ src/component/Item/Item.js
@@ -0,0 +1,68 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
+export const Item = ({ text, id, isCompleted, todos, setTodos }) => {
+  const handleDelete = (todoId) => {
+    const filteredTodos = todos.filter((todo) => {
+      if (todo.id !== todoId) {
+        return todo
+      } 
+      if (todos.length <= 1) {
+        localStorage.removeItem('todos')
+      }
+    });
+    setTodos([...filteredTodos]);
+    toast.error('todo ochirildi!');
+    console.log(todos);
+  };
+
+  const handleEdit = (id, text) => {
+    const newText = prompt('Yangi todoni kiriting', text);
+    const findedTodo = todos.find((todo) => todo.id === id);
+    findedTodo.text = newText;
+    setTodos([...todos]);
+    toast.warning('text almashtirildi!');
+  };
+
+  const handleChange = (todoId) => {
+    const findedTodo = todos.find((todo) => todo.id === todoId);
+    findedTodo.isCompleted = !findedTodo.isCompleted;
+    setTodos([...todos]);
+    if (findedTodo.isCompleted) {
+      toast.success('todo bajarildi!');
+    }
+  };
+
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+  return (
+    <li className='list-group-item d-flex align-items-center'>
+      <span>{id}</span>
+      <input
+        onChange={() => handleChange(id)}
+        checked={isCompleted}
+        type='checkbox'
+        className='form-check-input mx-3'
+      />
+      <strong
+        className={
+          isCompleted ? 'text-decoration-line-through text-success' : ''
+        }
+      >
+        {text}
+      </strong>
+      <button
+        onClick={() => handleEdit(id, text)}
+        className='btn btn-warning mx-3 ms-auto'
+      >
+        EDIT
+      </button>
+      <button
+        onClick={() => handleDelete(id)}
+        className='btn btn-danger'
+      >
+        DELETE
+      </button>
+    </li>
+  );
+};
